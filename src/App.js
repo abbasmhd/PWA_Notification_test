22ef9264ec5187de78c54009d71875ccb05505ba
Mo Abbas <mo.abbas@derivco.com.au>
2021-09-22 10:37:15 +1000

    React

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './App.css';
+import * as PusherPushNotifications from "@pusher/push-notifications-web";
+
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Task Manager</h3>
+    <Link to="/">Current Tasks</Link>
+    <Link to="/completed">Completed Tasks</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+    </p>
+    <ul className={props.status}>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+    </ul>
+  </div>
+);
+
+const CurrentTasks = () => (
+  <Template title="Current Tasks" status="Current"/>
+);
+
+const CompletedTasks = () => (
+  <Template title="Completed Tasks" status="Completed"/>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={CurrentTasks}/>
+          <Route path="/completed" component={CompletedTasks}/>
+        </div>
+      </BrowserRouter>
+
+      <button onClick={ () => this.startPwa()} >TEST</button>
+
+      </>
+    );
+  }
+
+  startPwa(){
+
+    console.log("startPwa");
+
+    const beamsClient = new PusherPushNotifications.Client({
+      instanceId: '47aee597-bc8e-4b71-8701-e7c53da0021f',
+    });
+  
+    beamsClient.start()
+      .then(() => beamsClient.addDeviceInterest('debug-hello'))
+      .then(() => console.log('Successfully registered and subscribed!'))
+      .catch(console.error);
+  
+  }
+}
+
+export default App;
